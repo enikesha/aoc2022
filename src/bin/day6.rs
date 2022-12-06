b5982b56c65ec804e709ef5e593978dc43e259d5
Innokentiy Enikeev <enikesha@gmail.com>
2022-12-06 11:38:39 +0100

    Day6

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,18 +1,25 @@
 use std::fs;
+use std::collections::HashSet;
 
 fn main() {
    let input = fs::read_to_string("day6.txt").unwrap();
    let mut start = 0;
+   let mut found = 0;
    loop {
-     if different(&input[start..]) {
+     if different(&input[start..start+4]) && (found & 1 == 0){
        println!("Part 1: {}", start + 4);
-       break;
+       found |= 1
      }
+     if different(&input[start..start+14]) && (found & 2 == 0) {
+       println!("Part 2: {}", start + 14);
+       found |= 2;
+     }
+     if found == 3 { break; }
      start += 1;
    }
 }
 
 fn different(str: &str) -> bool {
-  let s = str.as_bytes();
-  s[0] != s[1] && s[0] != s[2] && s[0] != s[3] && s[1] != s[2] && s[1] != s[3] && s[2] != s[3]
+  let bytes = str.as_bytes().iter().collect::<HashSet<_>>();
+  bytes.len() == str.len()
 }
